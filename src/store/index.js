552b92cab86a9d87ef19e1a6ff1cd77c7001a9bb
Natalia Savchuk <35775446+savchuknatalia@users.noreply.github.com>
2024-02-20 17:40:08 +0200

    added reset implementation

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,15 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import { songsReducer, addSong, removeSong } from "./slices/songsSlice";
+import { moviesReducer, addMovie, removeMovie } from "./slices/moviesSlice";
+import { reset } from "./actions";
 
-const songsSlice = createSlice({
-  name: 'song',
-  initialState: [],
-  reducers: {
-    addSong(state, action) {
-      state.push(action.payload);
-    },
-    removeSong(state, action) {
-      const index = state.indexOf(action.payload);
-      state.splice(index, 1);
-    } 
-  }
-});
 
 const store = configureStore({
   reducer: {
-    songs: songsSlice.reducer
+    songs: songsReducer,
+    movies: moviesReducer
   }
 });
 
 export { store };
-export const { addSong, removeSong } = songsSlice.actions;
+export { addMovie, removeMovie, addSong, removeSong, reset };
