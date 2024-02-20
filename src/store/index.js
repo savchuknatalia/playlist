610d211b1d24f975f824b9efd02dd65cefc954b8
Nataliia <35775446+savchuknatalia@users.noreply.github.com>
2024-02-20 10:17:36 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,8 @@ const songsSlice = createSlice({
       state.push(action.payload);
     },
     removeSong(state, action) {
-      //
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
     } 
   }
 });
@@ -20,5 +21,4 @@ const store = configureStore({
 });
 
 export { store };
-export const { addSong } = songsSlice.actions;
-// export const { removeSong } = songsSlice.actions;
+export const { addSong, removeSong } = songsSlice.actions;
